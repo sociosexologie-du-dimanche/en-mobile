ede9ec4ba82a6ed4f15733bb11a3ce9c23613919
Roamdam <romain.damian@ntymail.com>
2016-04-03 01:44:15 +0200

    ajuste graphiques

--- js/graphiques.js
+++ js/graphiques.js
@@ -384,3 +384,47 @@ $(function () {
         }]
     });
 });
+
+// cathos
+(function(){
+    // test 
+
+var imagesCathos = document.getElementById('imagesCathos'),     // Récupération des div contenant les iamges
+    imagesAthees = document.getElementById('imagesAthees'),
+    chiffre = document.getElementById('chiffreInfoCathos'),     // Récupération de la div contenant les chiffres
+    // Remplissage variables de texte contenant les chiffres
+    texteDepart = "How many catholic people have already had an homosexual intercourse ?",
+ 
+    cathoOui = "<img src='Images/cathoOui.png' width='15p' height='30'/>" ,
+    cathoNon = "<img src='Images/cathoNon.png' width='15p' height='30'/>",
+    atheeOui = "<img src='Images/atheeOui.png' width='15p' height='30'/>" ,
+    atheeNon = "<img src='Images/atheeNon.png' width='15p' height='30'/>";
+    
+var petitBloc , autreReligion ; // Variables utilisées dans la boucle initialisées ici
+
+// On va faire la même chose pour les deux lignes de bonhommes, donc on fait une fonction
+var alignerImages = function (religion,imageOui,imageNon,nombreOui) {
+    
+    // Les deux boucles for ajoutent en ligne les images en utilisant des span
+    for (i=0;i<nombreOui;i++) {
+        petitBloc = document.createElement('span') ;
+        petitBloc.setAttribute('id','span'+i) ;
+        petitBloc.setAttribute('class','oui') ;
+        religion.appendChild(petitBloc) ;
+        petitBloc.innerHTML = imageOui ;
+    }
+
+    for (i=0;i<20-nombreOui;i++) {
+        petitBloc = document.createElement('span') ;
+        petitBloc.setAttribute('id','span'+i) ;
+        petitBloc.setAttribute('class','non') ;
+        religion.appendChild(petitBloc) ;
+        petitBloc.innerHTML = imageNon ;
+    }
+
+
+chiffre.innerHTML = texteDepart ; // remplissage par le texte de départ à l'ouverture de la page
+
+alignerImages(imagesCathos,cathoOui,cathoNon,3) ;
+alignerImages(imagesAthees,atheeOui,atheeNon,4) ;
+})();
